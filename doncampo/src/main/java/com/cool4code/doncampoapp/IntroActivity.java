6e12ced30982e74a8a653be8277a90b7272bd43e
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2014-05-31 02:32:26 -0500

    Intro completed!
    I’ve been completed the Intro Activity

---
+++ doncampo/src/main/java/com/cool4code/doncampoapp/IntroActivity.java
@@ -0,0 +1,47 @@
+package com.cool4code.doncampoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+
+
+public class IntroActivity extends ActionBarActivity implements OnClickListener{
+    Button goToHome;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        goToHome= (Button) findViewById(R.id.intro_boton_ir_home);
+        goToHome.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.intro, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent goToHome= new Intent(IntroActivity.this, MainActivity.class);
+        startActivity(goToHome);
+    }
+}
