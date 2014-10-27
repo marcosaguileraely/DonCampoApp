eb7b8de1ed56b6383149bfba342e15313b84e062
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2014-10-27 09:05:49 -0500

    continue fixing stock

--- doncampo/src/main/java/com/cool4code/doncampoapp/SingUp.java
+++ doncampo/src/main/java/com/cool4code/doncampoapp/SingUp.java
@@ -1,9 +1,12 @@
 package com.cool4code.doncampoapp;
 
 import android.annotation.TargetApi;
+import android.app.ActionBar;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -13,6 +16,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.cool4code.doncampoapp.helpers.WebService;
@@ -44,6 +48,19 @@ public class SingUp extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_up);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ActionBar bar = getActionBar();
+        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#669900")));
+        int titleId;
+        int textColor = getResources().getColor(android.R.color.white);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            titleId = getResources().getIdentifier("action_bar_title", "id", "android");
+            TextView abTitle = (TextView) findViewById(titleId);
+            abTitle.setTextColor(textColor);
+        } else {
+            TextView abTitle = (TextView) getWindow().findViewById(android.R.id.title);
+            abTitle.setTextColor(textColor);
+        }
 
         name = (EditText) findViewById(R.id.register_name);
         documento = (EditText) findViewById(R.id.register_dni);
